b3934f320a654357a9e8586ec991894ca4a9b11e
PRIYAM ANAND <priyamanand77@gmail.com>
2024-10-05 02:05:58 +0530

    finished project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { Routes, Route } from "react-router-dom";
+import HeaderManager from "./components/HeaderManager";
+import LandingPage from "./components/LandingPage";
+import GetHospitalManager from "./components/GetHospitalManager";
+import BookAppointmentManager from "./components/BookAppointmentManager";
+import MainAboutUs from "./components/MainAboutUs"; 
+import AppointmentManager from "./components/AppointmentManager";
+import AppointmentManagerDefault from "./components/AppointmentManagerDefault";
+import MainSearchUser from "./components/MainSearchUser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{overflowY:"hidden"}}>
+      <Router>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<HeaderManager />} />
+          <Route path="/landing-page" element={<LandingPage />} />
+          <Route path="/show-hospitals" element={<GetHospitalManager />} />
+          <Route path="/book-appointment" element={<BookAppointmentManager />} />
+          <Route path ="/about" element={<MainAboutUs/>} />
+          <Route path ="/getAllAppointments" element={<AppointmentManagerDefault/>} />
+          <Route path ="/getAllAppointmentsNew" element={<AppointmentManager/>} />
+        
+          <Route path ="/test" element={<MainSearchUser/>} />
+            </Routes>
+      </Router>
+    
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import "bootstrap/dist/css/bootstrap.css";
+import 'react-slideshow-image/dist/styles.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
